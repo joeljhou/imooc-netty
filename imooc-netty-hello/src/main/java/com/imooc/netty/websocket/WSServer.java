d6b0b223ac16444e1c9792fd57954bd91209f031
zhouyu <zhouyu@gmy.com>
2021-11-16 18:10:34 +0800

    Initial commit

---
+++ imooc-netty-hello/src/main/java/com/imooc/netty/websocket/WSServer.java
@@ -0,0 +1,30 @@
+package com.imooc.netty.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class WSServer {
+    public static void main(String[] args) {
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup subGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(mainGroup,subGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new WSServerInitializer());
+
+            ChannelFuture future = server.bind(8888).sync();
+
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            mainGroup.shutdownGracefully();
+            subGroup.shutdownGracefully();
+        }
+    }
+}
