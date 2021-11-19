98d8c03ad04ea02561631f8f948d400e45ef8a4f
zhouyu <zhouyu@gmy.com>
2021-11-19 16:56:43 +0800

    创建一个Springboot项目（Spring，SpringMVC，Mybatisplus，Druid）

---
+++ im-weixin-netty/src/main/java/com/imooc/ImWeixinNettyApplication.java
@@ -0,0 +1,13 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImWeixinNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImWeixinNettyApplication.class, args);
+    }
+
+}
